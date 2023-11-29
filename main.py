8154af67a2c120dfaef9feb3d1181ac0fee9a92b
Nguyễn Phúc Khang <98502471+Khang2561@users.noreply.github.com>
2023-11-29 10:52:06 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from PIL import Image
 import pickle as pkl
-import numpy
+import numpy as np
 
 class_list = {'0':'Normal','1':'Pneumonia'}
 st.title('Pneumonia Detection')
@@ -14,3 +14,10 @@ image = st.file_uploader('Choose an image',type=(['png','jpg','jpeg]))
 if image is not None:
   image = Image.open(image)
   st.image(image,caption = 'Test image')
+
+  if st.button('Predict'):
+    image = image.resize((227*227*3,1))
+    vector = np.array(image)
+    label = st.write(model.predict(vector))
+    st.header('Result')
+    st.text(label)
