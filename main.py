c2f517c932c332a00d13982422e478f20aabb0d8
Nguyễn Phúc Khang <98502471+Khang2561@users.noreply.github.com>
2023-11-29 11:09:30 +0700

    Update main.py

--- main.py
+++ main.py
@@ -23,28 +23,3 @@ if image is not None:
 
     st.header('Result')
     st.text(class_list[label])
-import streamlit as st
-from PIL import Image
-import pickle as pkl
-import numpy as np
-
-class_list = {'0': 'Normal', '1': 'Pneumonia'}
-
-st.title('Pneumonia Detection')
-input = open('lrc_xray.pkl', 'rb')
-model = pkl.load(input)
-
-st.header('Upload an Image')
-image = st.file_uploader('Choose an image', type=(['png', 'jpg', 'jpeg']) )
-
-if image is not None:
-  image = Image.open(image)
-  st.image(image, caption = 'Test image')
-
-  if st.button('Predict'):
-    image = image.resize((227*227*3), 1)
-    vector = np.array(image)
-    label = str(st.write(model.predict(vector))[0])
-
-    st.header('Result')
-    st.text(class_list[label])
