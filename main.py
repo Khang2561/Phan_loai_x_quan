8606f0d051369f42cc73d46ac82c8338b3fff04b
Nguyễn Phúc Khang <98502471+Khang2561@users.noreply.github.com>
2023-11-29 11:00:38 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,20 +4,46 @@ import pickle as pkl
 import numpy as np
 
 class_list = {'0': 'Normal', '1': 'Pneumonia'}
-st.title('Pneumonia Detection')
 
+st.title('Pneumonia Detection')
 input = open('lrc_xray.pkl', 'rb')
 model = pkl.load(input)
 
-st.header('Upload an image')
-image = st.file_uploader('Choose an image', type=['png', 'jpg', 'jpeg'])
-if image is not None:
-    image = Image.open(image)
-    st.image(image, caption='Test image')
+st.header('Upload an Image')
+image = st.file_uploader('Choose an image', type=(['png', 'jpg', 'jpeg']) )
 
-    if st.button('Predict'):
-        image = image.resize((227, 227))  # Corrected the size
-        vector = np.array(image)
-        label = st.write(model.predict(vector))
-        st.header('Result')
-        st.text(label)
+if image is not None:
+  image = Image.open(image)
+  st.image(image, caption = 'Test image')
+
+  if st.button('Predict'):
+    image = image.resize((227*227*3), 1)
+    vector = np.array(image)
+    label = str(st.write(model.predict(vector))[0])
+
+    st.header('Result')
+    st.text(class_list[label])import streamlit as st
+from PIL import Image
+import pickle as pkl
+import numpy as np
+
+class_list = {'0': 'Normal', '1': 'Pneumonia'}
+
+st.title('Pneumonia Detection')
+input = open('lrc_xray.pkl', 'rb')
+model = pkl.load(input)
+
+st.header('Upload an Image')
+image = st.file_uploader('Choose an image', type=(['png', 'jpg', 'jpeg']) )
+
+if image is not None:
+  image = Image.open(image)
+  st.image(image, caption = 'Test image')
+
+  if st.button('Predict'):
+    image = image.resize((227*227*3), 1)
+    vector = np.array(image)
+    label = str(st.write(model.predict(vector))[0])
+
+    st.header('Result')
+    st.text(class_list[label])
